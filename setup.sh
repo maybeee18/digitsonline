6dbf99e37bab8e229b6bbf2d55c9c6cf6ee06620
Patryk Oleniuk <patryk.oleniuk@gmail.com>
2020-12-05 00:04:18 -0800

    heroku deploy bug

--- setup.sh
+++ setup.sh
@@ -1,10 +1,6 @@
 mkdir -p ~/.streamlit/
-echo "\
-[general]\n\
-email = \"patryk.oleniuk@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
+" > ~/.streamlit/config.toml
